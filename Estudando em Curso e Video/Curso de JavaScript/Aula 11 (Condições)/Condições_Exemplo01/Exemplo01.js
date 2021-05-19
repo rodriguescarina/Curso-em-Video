7c49d64898802a4a5e33356dbe61382ac9c79369
Carina Rodrigues <70609409+rodriguescarinaSI@users.noreply.github.com>
2021-05-19 16:27:59 -0300

    JavaScript Course

---
+++ Estudando em Curso e Video/Curso de JavaScript/Aula 11 (Condições)/Condições_Exemplo01/Exemplo01.js
@@ -0,0 +1,10 @@
+var vel = 78.2;
+
+console.log("A velocidade do seu carro é" + vel + "Km/h");
+
+if(vel > 60) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+
+} else {
+    console.log("Dirija sempre usando o cinto de segurança!");
+}
---
+++ Estudando em Curso e Video/Curso de JavaScript/aula 10 (DOM)/script.js
@@ -0,0 +1,16 @@
+ function Clicar() {
+    var a = window.document.getElementById('area');
+    a.innerText = 'clicou!'
+    a.style.background = 'red'
+  }
+  
+  function Entrar() {
+    var a = window.document.getElementById('area');
+    a.innerText = 'entrou!'
+  }
+  
+  function Sair() {
+    var a = window.document.getElementById('area');
+    a.innerText = 'saiu!'
+    a.style.background = 'green'
+  }
